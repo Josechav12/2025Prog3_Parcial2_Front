af58bc70a1f5832651b2897d65ddcd04bf12fe83
LautaroDellagiovanna <lau.dellag@gmail.com>
2025-11-30 22:55:57 -0300

    ADD: Creado el proyecto front a partir del parcial 1.

---
+++ frontend/js/carrito.js
@@ -0,0 +1,170 @@
+const contenedorCarrito = document.getElementById("contenedor-carrito");
+
+let carrito = [];
+
+async function obtenerProductos(){
+    let respuesta = await fetch("http://localhost:3000/api/products");
+    console.log(respuesta);
+    data = await respuesta.json();
+
+    if (respuesta.ok) {
+        console.log(data.message);
+
+        let productos = data.payload;
+
+        console.table(productos);
+
+        return productos;
+
+    } else {
+        alert("Error obteniendo productos")
+        return [];
+    }
+}
+
+/* Esta función la llaman los botones que pertenecen a los items del carrito.
+    Recibe por parámetro el id del producto que se quiere quitar, como puede haber
+    varios con el mismo id, se realiza una búsqueda para obtener el index del primer
+    caso donde los id coincidan, luego se remueve el elemento.
+    Luego se lo guarda en el storage local y finalmente se muestra el carrito actual
+ */
+function eliminarProducto(idProducto){
+    index = carrito.findIndex(item => item.id == idProducto);
+    carrito.splice(index, 1);
+    guardarCarritoEnLocalStorage();
+}
+
+/* Esta función le asigna al carrito un array vacío. De esta forma se borran todos los objetos que posee. 
+    Luego se lo guarda en el localstorage para también vaciar el mismo.*/
+function vaciarCarrito(){
+    carrito = [];
+    guardarCarritoEnLocalStorage();
+    mostrarCarrito();
+}
+
+/* Carga el carrito desde el localstorage. Como este se encuentra
+en formato texto, luego hay que parsearlo a objeto JSON. */
+function cargarCarritoDesdeLocalStorage(){
+    let textoCarrito = localStorage.getItem("carrito");
+
+    if (textoCarrito) {
+        carrito = JSON.parse(textoCarrito);
+    }
+}
+
+/* Función para guardar el carrito en el localstorage.
+Lo guarda en formate texto con el nombre de "carrito" */
+function guardarCarritoEnLocalStorage(){
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+/* Esta función introduce html en la sección contenedor-carrito.
+Recorre el array carrito e introduce el string html con la información requerida.
+Muestra el botón de vaciar carrito y, además, suma los precios del los productos del carrito y los muestra en el nav.*/
+function mostrarCarrito(){
+    console.log(carrito);
+    contenedorCarrito.innerHTML = `<h1>Carrito</h1><ul id="listado-carrito">`;
+    
+    carrito.forEach(prod => {
+        contenedorCarrito.innerHTML += `
+            <li class="bloque-item">
+                <img class="carrito-img" src="${prod.imagen}" alt="${prod.nombre}">
+                <p class="nombre-item">${prod.nombre} - $${prod.precio}</p>
+                <button class="boton-ordenar" onClick="eliminarProducto(${prod.id})">Eliminar</button>
+            </li>
+        `    
+    })
+
+    contenedorCarrito.innerHTML += "</ul>";
+
+    
+    if(carrito.length > 0){
+        contenedorCarrito.innerHTML += `
+            <div id="div-botones-carrito"> 
+                <button id="boton-vaciar-carrito" class="boton-carrito" onClick="vaciarCarrito()">Vaciar Carrito</button>
+                <button id="boton-comprar" class="boton-carrito" onClick="comprar()">Comprar</button> 
+            </div>`;
+        contenedorCarrito.innerHTML += `<h4 id="total-precio-carrito">Total: $${calcularPrecioTotal()}</h4>`;
+    }
+}
+
+function mostrarAgradecimiento() {
+    contenedorCarrito.innerHTML = `<h1>Gracias por su compra</h1>`;
+}
+
+function calcularPrecioTotal() {
+    let precioTotal = 0;
+    precioTotal = carrito.reduce((precioTotal, item) => { return precioTotal + item.precio;}, 0);
+    return precioTotal;
+}
+
+/* Esta función la llaman los botones que pertenecen a los items del carrito.
+    Recibe por parámetro el id del producto que se quiere quitar, como puede haber
+    varios con el mismo id, se realiza una búsqueda para obtener el index del primer
+    caso donde los id coincidan, luego se remueve el elemento.
+    Luego se lo guarda en el storage local y finalmente se muestra el carrito actual
+ */
+function eliminarProducto(idProducto){
+    index = carrito.findIndex(item => item.id == idProducto);
+    carrito.splice(index, 1);
+    guardarCarritoEnLocalStorage();
+    mostrarCarrito();
+}
+
+async function comprar() {
+
+    let comprarProductos_form = document.getElementById("campo-nombre-cliente");
+
+    let formData = new FormData(comprarProductos_form);
+    let data = Object.fromEntries(formData.entries());
+
+    if (!nombre) {
+        
+    }
+
+    data['precio'] = calcularPrecioTotal();
+    data['productos'] = [];
+
+    carrito.forEach(prod => {
+        data['productos'].push(prod.id);
+    });
+
+    console.log(JSON.stringify(data))
+
+    let respuesta = await fetch("http://localhost:3000/api/tickets/create", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    console.log(respuesta);
+    
+    data = await respuesta.json();
+    console.log(data)
+
+    if (respuesta.ok) {
+        console.log(data.message);
+
+        vaciarCarrito();
+        mostrarAgradecimiento();
+        return data;
+
+    } else {
+        alert("Error creando ticket")
+        return [];
+    }
+
+}
+
+/* Función que llama a todas la funciones necesariar para mostra la página por primera vez. */
+async function initBienvenida(){
+    /* Se crea el array de productos con los campos requeridos */
+
+    productos = await obtenerProductos();
+    cargarCarritoDesdeLocalStorage();
+    mostrarCarrito();
+}
+
+initBienvenida();
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,44 +1,138 @@
-let contenedorProductos = document.getElementById("contenedor-productos");
+/* -------------
+    Parte 2 
+-----------------*/
+/* Se crea el objeto alumno */
+let alumno = {
+    dni: "40479981",
+    nombre: "Lautaro",
+    apellido: "Dellagiovanna"
+};
 
-async function obtenerProductos() {
-    try {
-        // Hacemos la peticion get a la url de nuestra api rest para obtener los productos
-        // Ojo! Para evitar que la politica de seguridad CORS bloquee nuestra peticion fetch a esa url, necesitamos habilitar CORS desde nuestra API Rest
-        let respuesta = await fetch("http://localhost:3000/api/products");
-        let respuestaFormato = await respuesta.json();
+/* Se guardan las variables del DOM requeridas para luego modificar su innerHTML o añadirle eventos */
+const navContadorProductosCarrito = document.getElementById("nav-contador-prod-carrito");
+const navNombreAlumno = document.getElementById("nav-nombre-alumno");
+const contenedorProductos = document.getElementById("contenedor-productos");
+const barraBusqueda = document.getElementById("barra-busqueda");
+const contenedorCarrito = document.getElementById("contenedor-carrito");
 
-        let productos = respuestaFormato.payload;
+/* Se añade el evento de keyup a la barra de busqueda, la cual llamará a la función filtrarProductos */
+barraBusqueda.addEventListener("keyup", filtrarProductos);
 
-        console.table(productos);
+/* Se crea el array vacio de carrito donde se irán añadiendo al presionar el botón correspondiente */
+let carrito = [];
 
-        mostrarProductos(productos);
 
-    } catch (error) {
-        console.error(error);
+/* -------------
+    Parte 1  
+-----------------*/
+/* Esta función muestra el nombre, apellido y dni del alumno en html del elemento navNombreAlumno 
+    y en la consola. Primero se concatena en una varible string para luego asignarla al html y realizar
+    el console.log*/
+function imprimirDatosAlumno(){
+    let nombreAlumno = `${alumno.nombre} ${alumno.apellido} - ${alumno.dni}`
+    navNombreAlumno.innerHTML = nombreAlumno;
+    console.log(nombreAlumno);
+}
+
+/* Esta función añade html a la sección de contenedor de productos.
+    Primero borra por completo el html asignadoles un string vacío.
+    Luego recorre el array de productos y por cada uno añade al string
+    un string que representa html con la información de cada producto. */
+function mostrarProductos(array){
+    contenedorProductos.innerHTML = "";
+    
+    array.forEach(prod => {
+        contenedorProductos.innerHTML += `
+            <div class="card-producto">
+                <img src="${prod.imagen}" alt="${prod.nombre}">
+                <div class="card-producto-info">
+                    <h3>${prod.nombre}</h3>
+                    <p>$${prod.precio}</p>
+                    <button onClick="agregarACarrito(${prod.id})">Agregar al carrito</button>
+                </div>
+            </div>
+        `
+    })
+    
+}
+
+/* Esta función la llama el evento keyup asignado a la barra de busqueda.
+    Obtiene el texto que se introdujo en la barra para luego realizar un
+    filtrado de productos, lo cual devuelve aquellos un array con aquellos 
+    cuyo nombre contenga el texto introducido en la barra. 
+    Finalmente los muestra para refrescar el html de la sección de productos
+    sólo con el array ya filtrado. */
+function filtrarProductos(){
+    textoBuscado = barraBusqueda.value.toLowerCase();
+    
+    prodBuscados = productos.filter(prod => 
+        prod.nombre.toLowerCase().includes(textoBuscado)
+    );
+
+    mostrarProductos(prodBuscados);
+}
+
+/* Esta función la llaman los botones que pertenecen a los productos.
+    Recibe por parámetro el id del producto que se quiere añadir, el
+    cual se añade luego de buscarlo por id en el array de productos.
+    Luego se lo guarda en el storage local y finalmente se muestra el carrito actual
+ */
+function agregarACarrito(idProducto){
+    carrito.push(productos.find(p => p.id == idProducto));
+    guardarCarritoEnLocalStorage();
+    actualizarContadorCarrito();
+}
+
+
+function actualizarContadorCarrito(){
+    navContadorProductosCarrito.innerHTML = `Carrito: ${carrito.length} Productos`;
+}
+
+/* Función para guardar el carrito en el localstorage.
+Lo guarda en formate texto con el nombre de "carrito" */
+function guardarCarritoEnLocalStorage(){
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+/* Carga el carrito desde el localstorage. Como este se encuentra
+en formato texto, luego hay que parsearlo a objeto JSON. */
+function cargarCarritoDesdeLocalStorage(){
+    let textoCarrito = localStorage.getItem("carrito");
+
+    if (textoCarrito) {
+        carrito = JSON.parse(textoCarrito);
     }
 }
 
 
+async function obtenerProductos(){
+    let respuesta = await fetch("http://localhost:3000/api/products");
+    console.log(respuesta);
+    data = await respuesta.json();
 
-function mostrarProductos(productos) {
-    let htmlProductos = "";
+    if (respuesta.ok) {
+        console.log(data.message);
 
-    productos.forEach(prod => {
-        htmlProductos += `
-            <div class="card-producto">
-                <img src="${prod.imagen}" alt="${prod.nombre}">
-                <h3>${prod.nombre}</h3>
-                <p>$${prod.precio}</p>
-            </div>
-        `;
-    });
+        let productos = data.payload;
 
-    contenedorProductos.innerHTML = htmlProductos;
+        console.table(productos);
+
+        return productos;
+
+    } else {
+        alert("Error obteniendo productos")
+        return [];
+    }
 }
 
-function init() {
-    obtenerProductos();
-}
+/* Función que llama a todas la funciones necesariar para mostra la página por primera vez. */
+async function init(){
+    /* Se crea el array de productos con los campos requeridos */
 
+    productos = await obtenerProductos();
+    mostrarProductos(productos);
+    cargarCarritoDesdeLocalStorage();
+    actualizarContadorCarrito();
+}
 
 init();
